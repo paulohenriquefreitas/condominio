5b858994637735a6d193e5946458e574247f7c75
Paulo Freitas <paulo.freitas@ideais.com.br>
2017-02-18 12:44:03 -0200

    Implementação de um novo layout

--- src/main/java/com/condominio/CondominioApplication.java
+++ src/main/java/com/condominio/CondominioApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class CondominioApplication extends SpringBootServletInitializer{
 	
-
-	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CondominioApplication.class);
