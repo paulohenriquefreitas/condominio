49830ff522bf66bcab409c6d7865fb27f86595d6
Danilo <danilorio2@hotmail.com>
2017-02-21 15:50:07 -0300

    fornecedor

---
+++ src/main/java/com/Fornecedor/Fornecedor.java
@@ -0,0 +1,15 @@
+package com.Fornecedor;
+
+public class Fornecedor {
+
+ String  Nome_Fornecedor;
+
+public String getNome_Fornecedor() {
+	return Nome_Fornecedor;
+}
+
+public void setNome_Fornecedor(String nome_Fornecedor) {
+	Nome_Fornecedor = nome_Fornecedor;
+} 
+	
+}
