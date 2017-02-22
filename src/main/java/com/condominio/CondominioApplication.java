36b4d7e0b5f33ff2e844d7de81d2a26ef72e299d
Paulo Freitas <paulo.freitas@ideais.com.br>
2017-02-22 00:03:07 -0300

    Formulários e controllers

--- src/main/java/com/condominio/CondominioApplication.java
+++ src/main/java/com/condominio/CondominioApplication.java
@@ -4,15 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.controller"})
 @SpringBootApplication
 public class CondominioApplication extends SpringBootServletInitializer{
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CondominioApplication.class);
-	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CondominioApplication.class, args);
 	}
