83ea8145cc60015dab0575417f56d91aa8ef75b2
Paulo Freitas <paulo.freitas@ideais.com.br>
2017-03-14 22:17:53 -0300

    Removendo erros

---
+++ condominio/src/test/java/com/condominio/CondominioApplicationTests.java
@@ -0,0 +1,14 @@
+package com.condominio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class CondominioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
