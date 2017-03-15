bc2c38bd7fe5301762f67e705bd9989ba70f3085
Danilo <danilorio2@hotmail.com>
2017-03-14 21:17:03 -0300

    corrigir

--- src/main/java/com/condominio/CondominioApplication.java
+++ src/main/java/com/condominio/CondominioApplication.java
@@ -2,14 +2,19 @@ package com.condominio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@ComponentScan(basePackages = {"com.controller"})
 @SpringBootApplication
-public class CondominioApplication extends SpringBootServletInitializer{
+@Controller
+public class CondominioApplication{
 	
+	@RequestMapping("/")
+	@ResponseBody
+	public String index(){
+        return "funciona?";
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CondominioApplication.class, args);
--- src/main/java/com/dao/MoradorDao.java
+++ src/main/java/com/dao/MoradorDao.java
@@ -8,19 +8,5 @@ import com.model.Morador;
 
 public class MoradorDao {
 
-	public void persist(Morador morador){
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("Condominio");
-	EntityManager em = emf.createEntityManager();
-	em.getTransaction().begin();
-	try {
-		em.persist(morador);
-        em.getTransaction().commit();
-	} catch (Exception e) {
-		e.printStackTrace();
-		em.getTransaction().rollback();
-	}finally {
-		em.close();
-		
-	   }
-	}
+	
 }
