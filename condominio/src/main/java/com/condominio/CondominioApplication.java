e69c4dd5324a388010b6eb458c0746d3065f889f
Danilo <danilorio2@hotmail.com>
2017-05-09 21:50:48 -0300

    config banco

--- condominio/src/main/java/com/condominio/CondominioApplication.java
+++ condominio/src/main/java/com/condominio/CondominioApplication.java
@@ -40,7 +40,7 @@ public class CondominioApplication extends SpringBootServletInitializer {
 	public DataSource dataSource(Environment environment) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/condominio");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/Condominio");
         dataSource.setUsername("root");
         dataSource.setPassword("root");
         
